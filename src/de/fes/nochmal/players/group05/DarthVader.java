22fc01712685a686aeae629acfe5e587ded75cf0
Dima <dimon04228@gmail.com>
2026-05-21 21:56:45 +0200

    1

--- src/de/fes/nochmal/players/group05/DarthVader.java
+++ src/de/fes/nochmal/players/group05/DarthVader.java
@@ -6,7 +6,7 @@ import de.fes.nochmal.model.Sheet;
 import de.fes.nochmal.players.AbstractComputerPlayer;
 import de.fes.nochmal.players.PlayerUtils;
 import de.fes.nochmal.util.log.Log;
-
+import de.fes.nochmal.model.Square;
 
 public class DarthVader extends AbstractComputerPlayer {
 
@@ -28,9 +28,50 @@ public class DarthVader extends AbstractComputerPlayer {
 
 	@Override
 	public PlayerChoice playTurn(Sheet sheet, int roundNumber, boolean firstPlayerInRound, Dice dice, Log log) {
+		Square[][] squares = sheet.getSquares();
 		
 		PlayerChoice[] possiblePlayerChoices = PlayerUtils.getPossiblePlayerChoices(sheet, dice, log);
+		
+		boolean isAFilled = true;
+		
+		for (int i = 0; i < sheet.getNumberOfRows(); i++) {
+			if (!squares[0][i].isMarked()) {
+				isAFilled = false; 
+				break;
+			}
+		}
+		
+		System.out.println(roundNumber + " " + isAFilled);
+		
+		
+		if (isAFilled == false) {
+			System.out.println("A");
+			return possiblePlayerChoices[0];
+		}
+		
+		
+		boolean isOFilled = true;
+		int lastColumnIndex = sheet.getNumberOfColumns() - 1;
+		
+		for (int f = 0; f < sheet.getNumberOfRows(); f++) {
+			if (!squares[lastColumnIndex][f].isMarked()) {
+				isOFilled = false; 
+				break;
+			}
+		}
+		
+		if (isOFilled == false) {
+			int lastChoiceIndex = possiblePlayerChoices.length - 1;
+			
+			System.out.println("O");
+			return possiblePlayerChoices[lastChoiceIndex];
+		}
+		else {
+			System.out.println("Nein");
+			return possiblePlayerChoices[0];
+		}
+		
+		//System.out.println(dice);
 
-		return possiblePlayerChoices[0];
 	}
 }
