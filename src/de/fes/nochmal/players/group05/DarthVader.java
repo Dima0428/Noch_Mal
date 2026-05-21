693aa97ff4925488e6e1e63da02af46d9937a858
Dima <dimon04228@gmail.com>
2026-05-21 09:40:33 +0200

    2

---
+++ src/de/fes/nochmal/players/group05/DarthVader.java
@@ -0,0 +1,33 @@
+package de.fes.nochmal.players.group05;
+
+import de.fes.nochmal.game.PlayerChoice;
+import de.fes.nochmal.model.Dice;
+import de.fes.nochmal.model.Sheet;
+import de.fes.nochmal.players.AbstractComputerPlayer;
+import de.fes.nochmal.util.log.Log;
+
+public class DarthVader extends AbstractComputerPlayer {
+
+	public static final String Id = "DarthVader";
+	
+	public DarthVader() {
+		super(Id, true);
+	}
+
+	@Override
+	public boolean isComputerPlayer() {
+		return true;
+	}
+
+	@Override
+	public void initialize(Sheet sheet, Log log) {
+		//Nichts zu tun
+		
+	}
+
+	@Override
+	public PlayerChoice playTurn(Sheet sheet, int roundNumber, boolean firstPlayerInRound, Dice dice, Log log) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
