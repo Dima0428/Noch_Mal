b00cb5d7702dae5c1ab118711901c449c78c289e
Dima <dimon04228@gmail.com>
2026-05-28 10:15:40 +0200

    1

--- src/de/fes/nochmal/players/group05/DarthVader.java
+++ src/de/fes/nochmal/players/group05/DarthVader.java
@@ -1,3 +1,4 @@
+
 package de.fes.nochmal.players.group05;
 
 import de.fes.nochmal.game.PlayerChoice;
@@ -12,7 +13,7 @@ import de.fes.nochmal.model.Square;
 
 public class DarthVader extends AbstractComputerPlayer {
 
-	public static final String Id = "_mit10_DarthVader";
+	public static final String Id = "DarthVader";
 	
 	public DarthVader() {
 		super(Id, true);
@@ -39,46 +40,10 @@ public class DarthVader extends AbstractComputerPlayer {
 			return possiblePlayerChoices[0];
 		}
 		
-		System.out.println(sheet);
 		
 		// Basispriorität: von der Mitte zu den Rändern
 		//int[] columnPriority = {7, 8, 6, 9, 5, 10, 4, 11, 3, 12, 2, 13, 1, 14, 0};
 		int[] columnPriority = {7, 6, 8, 5, 9, 4, 10, 3, 11, 2, 12, 1, 13, 0, 14};
-		
-		// Wenn wir uns in der Expansionsphase befinden (bis Runde 10)
-		// und eine der Seiten bereits erreicht wurde,
-		// ändern wir die Gewichtung der Spalten
-		if (roundNumber <= 10) {
-			
-			boolean leftEdgeReached = false;  // Spalte 0 (A) erreicht?
-			boolean rightEdgeReached = false; // Spalte 14 (O) erreicht?
-			
-			Square[] markedSquares = PlayerUtils.getMarkedSquares(sheet);
-			for (Square square : markedSquares) {
-				int col = square.getColumn();
-				if (col == 0 || col == 1 || col == 2 || col == 3) {
-					leftEdgeReached = true;
-				}
-				if (col == 14 || col == 13 || col == 12 || col == 11) {
-					rightEdgeReached = true;
-					
-				}
-			}
-			
-			
-			
-			if (leftEdgeReached && !rightEdgeReached) {
-				// Linke Seite bereits erreicht,
-				// linke Hälfte künstlich abwerten,
-				// damit der Bot nach rechts expandiert
-				columnPriority = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-			} else if (rightEdgeReached && !leftEdgeReached) {
-				// Rechte Seite bereits erreicht,
-				// rechte Hälfte abwerten,
-				// damit der Bot nach links expandiert
-				columnPriority = new int[]{14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-			}
-		}
 
 		PlayerChoice bestChoice = null;
 		
@@ -138,34 +103,6 @@ public class DarthVader extends AbstractComputerPlayer {
 			if (currentIsJoker && !bestIsJoker) {
 				continue;
 			}
-			
-			if (roundNumber <= 10) {
-				// --- MODUS BIS RUNDE 10 ---
-				if (currentColumnPriority > bestColumnPriority) {
-					bestChoice = choice;
-					bestColumnPriority = currentColumnPriority;
-					bestSquaresCount = currentSquaresCount;
-				} 
-				else if (currentColumnPriority == bestColumnPriority) {
-					if (currentSquaresCount > bestSquaresCount) {
-						bestChoice = choice;
-						bestSquaresCount = currentSquaresCount;
-					}
-				}
-			} else {
-				// --- MODUS NACH RUNDE 10 ---
-				if (currentSquaresCount > bestSquaresCount) {
-					bestChoice = choice;
-					bestSquaresCount = currentSquaresCount;
-					bestColumnPriority = currentColumnPriority;
-				} 
-				else if (currentSquaresCount == bestSquaresCount) {
-					if (currentColumnPriority > bestColumnPriority) {
-						bestChoice = choice;
-						bestColumnPriority = currentColumnPriority;
-					}
-				}
-			}
 		}
 		
 		return bestChoice != null ? bestChoice : possiblePlayerChoices[0];
@@ -175,3 +112,4 @@ public class DarthVader extends AbstractComputerPlayer {
 
 	}
 }
+
